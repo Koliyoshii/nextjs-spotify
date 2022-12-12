67f9ecc84caae14608f9549da7e7a9cb89ff1355
Oliver Kusch <oliver.kusch@isgus.de>
2022-12-12 16:04:38 +0100

    Fix Login error

--- middleware.js
+++ middleware.js
@@ -2,7 +2,13 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.JWT_SECRET,
+    secureCookie:
+      process.env.NEXTAUTH_URL?.startsWith("https://") ??
+      !!process.env.VERCEL_URL,
+  });
 
   const { pathname } = req.nextUrl;
 
@@ -11,14 +17,14 @@ export async function middleware(req) {
   //2) the token exists
   if (pathname.includes("/api/auth") || token) {
     //proceed to the Home Page
-    console.log("ACCESS TOKEN IS VALID")
+    console.log("ACCESS TOKEN IS VALID");
     return NextResponse.next();
   }
 
   const url = req.nextUrl.clone();
   //Redirect to login if they dont have a token
   if (!token && pathname !== "/login") {
-    console.log("NO ACCESS TOKEN FOUND")
+    console.log("NO ACCESS TOKEN FOUND");
     url.pathname = "/login";
     console.log("Redirecting...");
     return NextResponse.redirect(url);
